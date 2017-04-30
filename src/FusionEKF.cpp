4f5eaa10044eaa2e7f59fc56ec6bacf15f0ae646
asheeshv <asheeshv@gmail.com>
2017-04-29 23:47:43 -0500

    Modified files for the EKF project

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -30,14 +30,35 @@ FusionEKF::FusionEKF() {
   R_radar_ << 0.09, 0, 0,
         0, 0.0009, 0,
         0, 0, 0.09;
-
-  /**
-  TODO:
-    * Finish initializing the FusionEKF.
-    * Set the process and measurement noises
-  */
-
-
+    
+    H_laser_ << 1, 0, 0, 0,
+    0, 1, 0, 0;
+    
+    Hj_ << 1, 1, 0, 0,
+    1, 1, 0, 0,
+    1, 1, 1, 1;
+    
+    //the initial transition matrix F_
+    ekf_.F_ = MatrixXd(4, 4);
+    ekf_.F_ << 1, 0, 1, 0,
+    0, 1, 0, 1,
+    0, 0, 1, 0,
+    0, 0, 0, 1;
+    
+    ekf_.P_ = MatrixXd(4, 4);
+    ekf_.P_ << 1000, 0, 0, 0,
+    0, 1000, 0, 0,
+    0, 0, 1000, 0,
+    0, 0, 0, 1000;
+    
+    ekf_.F_ = MatrixXd(4, 4);
+    ekf_.F_ << 1, 0, 1, 0,
+    0, 1, 0, 1,
+    0, 0, 1, 0,
+    0, 0, 0, 1;
+    
+    noise_ax = 9;
+    noise_ay = 9;
 }
 
 /**
@@ -52,28 +73,32 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    *  Initialization
    ****************************************************************************/
   if (!is_initialized_) {
-    /**
-    TODO:
-      * Initialize the state ekf_.x_ with the first measurement.
-      * Create the covariance matrix.
-      * Remember: you'll need to convert radar from polar to cartesian coordinates.
-    */
-    // first measurement
+
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 1, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+        
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+        float rho = measurement_pack.raw_measurements_(0);
+        float phi = measurement_pack.raw_measurements_(1);
+        float ro_dot = measurement_pack.raw_measurements_(2);
+        
+        float px = rho * cos(phi);
+        float py = rho * sin(phi);
+        float vx = ro_dot * cos(phi);
+        float vy = ro_dot * sin(phi);
+        
+        ekf_.x_ << px, py, vx, vy;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-      /**
-      Initialize state.
-      */
+        ekf_.x_ << measurement_pack.raw_measurements_(0), measurement_pack.raw_measurements_(1), 0, 0;
     }
-
+      previous_timestamp_ = measurement_pack.timestamp_;
+      
     // done initializing, no need to predict or update
     is_initialized_ = true;
     return;
@@ -90,23 +115,42 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
-
-  ekf_.Predict();
-
+    //compute the time elapsed between the current and previous measurements
+    float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+    previous_timestamp_ = measurement_pack.timestamp_;
+    
+    ekf_.F_(0, 2) = dt;
+    ekf_.F_(1, 3) = dt;
+    
+    float dt2 = pow(dt, 2);
+    float dt3 = pow(dt, 3);
+    float dt4 = pow(dt, 4);
+    
+    ekf_.Q_ = MatrixXd(4, 4);
+    ekf_.Q_ << dt4/4*noise_ax, 0, dt3/2*noise_ax, 0,
+    0, dt4/4*noise_ay, 0, dt3/2*noise_ay,
+    dt3/2*noise_ax, 0, dt2*noise_ax, 0,
+    0, dt3/2*noise_ay, 0, dt2*noise_ay;
+    
+    ekf_.Predict();
   /*****************************************************************************
    *  Update
    ****************************************************************************/
-
-  /**
-   TODO:
-     * Use the sensor type to perform the update step.
-     * Update the state and covariance matrices.
-   */
-
+    
+    VectorXd x_new = measurement_pack.raw_measurements_;
+    
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+      if ((x_new(0) != 0) || (x_new(1) != 0)) {
+          ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+          ekf_.R_ = R_radar_;
+          ekf_.UpdateEKF(x_new);
+      }
   } else {
     // Laser updates
+      ekf_.H_ = H_laser_;
+      ekf_.R_ = R_laser_;
+      ekf_.Update(x_new);
   }
 
   // print the output
